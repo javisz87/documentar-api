18e208aefe70140c48d0aaf36bfccf9c1fcfc495
javier <javiszumilo87@gmail.com>
2024-05-28 21:21:37 -0300

    subo-todo2

--- src/config/config.dotenv.js
+++ src/config/config.dotenv.js
@@ -6,7 +6,5 @@ module.exports = {
     clientId: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     persistence: process.env.PERSISTENCE,
-    twilio_account_sid: process.env.TWILIO_ACCOUNT_SID,
-    twilio_auth_token: process.env.TWILIO_AUTH_TOKEN,
-    twilio_sms_number: process.env.TWILIO_SMS_NUMBER
+  
 }
