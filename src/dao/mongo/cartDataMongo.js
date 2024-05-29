ec6237cf741bedfa26fc89568ab11398a0eead9e
javier <javiszumilo87@gmail.com>
2024-05-28 21:09:40 -0300

    subo-todo

---
+++ src/dao/mongo/cartDataMongo.js
@@ -0,0 +1,25 @@
+const { cartModel } = require('./models/cartModel');
+
+class CartMongo {
+    constructor() {
+    }
+
+    get = async () => {
+        let cart = await cartModel.find().populate('products_list.product')
+        return cart
+    }
+
+    getById = async (cid) => await cartModel.findOne({_id: cid}).populate('products_list.product')
+
+    create = async (cart) => {
+        return await cartModel.create(cart)
+    }
+
+    update = async (cid, cart) => {
+        return await cartModel.updateOne({ _id: cid }, cart)
+    }
+
+}
+
+
+module.exports = CartMongo
---
+++ src/routes/products.router.js
@@ -0,0 +1,23 @@
+const {Router} = require('express')
+const router = Router()
+const authJwt = require('../middlewares/authJwt')
+const productsControllers = require('../controllers/productsControllers')
+
+//Obtener lista de productos
+router.get("/", productsControllers.getProducts)
+
+//Obtener producto por ID
+router.get("/:pid", productsControllers.getProductById)
+
+//Agregar productos
+router.post("/", authJwt.verifyToken, authJwt.isAdminOrPremium, productsControllers.createProducts)
+
+//Actualizar un producto
+router.put("/:pid", authJwt.verifyToken, authJwt.isAdminOrPremium, productsControllers.updateProduct)
+
+//Eliminar producto por su ID.
+router.delete("/:pid", authJwt.verifyToken, authJwt.isAdminOrPremium, productsControllers.deleteProduct)
+
+module.exports = router;
+
+
